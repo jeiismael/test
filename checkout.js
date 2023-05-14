094051c4da4fec9148fe0d2803c8fd1302453c69
Jerry Joy ismael <jeiismael@gmail.com>
2023-05-14 08:00:03 +0800

    added test message

--- checkout.js
+++ checkout.js
@@ -13,3 +13,4 @@ console.log("Tax Rate: " + taxRate * 100 + "%") // will show tax rate in percent
 console.log("Subtotal: $" + subTotal) // calculation of sub total
 console.log("Tax Amount: $" + taxAmount) //calculation of tax amount per purchase
 console.log("Total Amount: $" + totalAmount) // total amount to be paid including tax
+console.log("test")
